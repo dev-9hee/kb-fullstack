3ef0045a9d90ddb7a5b811c43ff958c6b4372626
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-08-10 10:41:00 +0000

    20240810 보물지도 문제

--- 04_java/algorithmstudy/src/codingtest/week6/전력망을둘로나누기/Solution.java
+++ 04_java/algorithmstudy/src/codingtest/week6/전력망을둘로나누기/Solution.java
@@ -1,5 +1,54 @@
 package codingtest.week6.전력망을둘로나누기;
 
-public class Solution {
+import java.util.*;
 
+public class Solution {
+    int answer; // 노드 수 차이의 최소값을 저장
+    public int solution(int n, int[][] wires) { // n : 송전탑의 개수, wires : 전선 정보
+        answer = n; // 처음엔 송전탑의 개수 n으로 초기화
+        // 주어진 엣지 데이터를 사용하기 쉽게 가공
+        // 그래프 구현
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for (int i=1; i<=n; i++) {
+            graph.put(i, new ArrayList<>());
+        }
+        // 그래프 표현
+        for (int[] w : wires) {
+            graph.get(w[0]).add(w[1]);
+            graph.get(w[1]).add(w[0]);
+        }
+
+        // dfs 탐색
+        boolean[] visited = new boolean[n+1];
+        dfs(graph, visited, 1, n); // 시작 노드를 1로 설정하고 dfs 탐색을 시작
+
+        return answer;
+    }
+
+    public int dfs(Map<Integer, List<Integer>> graph, boolean[] visited, int cur, int n) {
+        int count = 1; // 현재 노드를 포함한 서브트리의 노드 수, 1로 초기화
+        visited[cur] = true; // 현재 노드 방문 표시
+
+        // 인접 노드 탐색
+        for (int next : graph.get(cur)) {
+            if (!visited[next]) {
+                // 아직 방문하지 않은 노드에 대해 재귀적으로 dfs 를 호출하고,
+                // 해당 노드에서 반환된 서브트리의 노드 수를 count 에 더함
+                count += dfs(graph, visited, next, n);
+            }
+        }
+
+        // 최적값 업데이트
+        // 전선 하나를 제거했을 때 두 전력망으로 나뉘는 상황에서 각 부분의 노드 수 차이를 의미
+         answer = Math.min(answer, Math.abs(n - count * 2));
+
+        // 자신 아래의 노드 수를 재귀적으로 반환
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int result = solution.solution(9, new int[][]{{1,3},{2,3},{3,4},{4,5},{4,6},{4,7},{7,8},{7,9}});
+        System.out.println(result);
+    }
 }
---
+++ 04_java/algorithmstudy/src/codingtest/week7/보물지도/Solution.java
@@ -0,0 +1,60 @@
+package codingtest.week7.보물지도;
+
+import java.util.*;
+
+public class Solution {
+    public int solution(int n, int m, int[][] hole) {
+        //✅ 함정 위치를 표시한다.
+        boolean[][] trap = new boolean[n+1][m+1];
+        for (int[] h : hole) {
+            trap[h[0]][h[1]] = true;
+        }
+
+        boolean[][][] visited = new boolean[n+1][m+1][2];
+        Queue<int[]> queue = new ArrayDeque<>();
+        visited[1][1][0] = true;
+        queue.add(new int[]{ 1, 1, 0, 0 });
+
+        int[] dr = { 0, 1, 0, -1 };
+        int[] dc = { 1, 0, -1, 0 };
+
+        //✅ BFS 탐색을 수행한다.
+        while (!queue.isEmpty()) {
+            int[] cur = queue.remove();
+            int r = cur[0], c = cur[1], jumped = cur[2], dist = cur[3];
+
+            //✅ 목적지에 도착했다면 최단거리를 반환한다.
+            if (r == n && c == m) {
+                return dist;
+            }
+
+            //✅ 한칸 이동하는 경우를 탐색한다.
+            for (int d = 0; d < 4; d++) {
+                int nr = r + dr[d], nc = c + dc[d];
+
+                if (nr >= 1 && nr <= n && nc >= 1 && nc <= m) {
+                    if (!visited[nr][nc][jumped] && !trap[nr][nc]) {
+                        visited[nr][nc][jumped] = true;
+                        queue.add(new int[]{ nr, nc, jumped, dist + 1 });
+                    }
+                }
+            }
+
+            //✅ 점프를 뛰지 않은 경우에는 점프를 뛰는 경우도 탐색한다.
+            if (jumped == 0) {
+                for (int d = 0; d < 4; d++) {
+                    int nr = r + 2 * dr[d], nc = c + 2 * dc[d];
+
+                    if (nr >= 1 && nr <= n && nc >= 1 && nc <= m) {
+                        if (!visited[nr][nc][1] && !trap[nr][nc]) {
+                            visited[nr][nc][1] = true;
+                            queue.add(new int[]{ nr, nc, 1, dist + 1 });
+                        }
+                    }
+                }
+            }
+        }
+
+        return -1;
+    }
+}
