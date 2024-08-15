669345b52ecd22198eb0282d1d18d04372c0607e
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-08-15 23:51:18 +0900

    20240815

--- 08_Spring/ex03/src/main/java/org/scoula/ex03/controller/SampleController.java
+++ 08_Spring/ex03/src/main/java/org/scoula/ex03/controller/SampleController.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 
 @Controller
