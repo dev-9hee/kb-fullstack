87bc7c78b80ae48ec13fba1c1ae06e612486ab67
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-08-16 16:38:37 +0900

    20240816 데이터베이스 연동

--- 08_Spring/ex04/src/main/java/org/scoula/config/RootConfig.java
+++ 08_Spring/ex04/src/main/java/org/scoula/config/RootConfig.java
@@ -1,7 +1,32 @@
 package org.scoula.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+import javax.sql.DataSource;
 
 @Configuration
+@PropertySource({"classpath:/application.properties"})
 public class RootConfig {
+    @Value("${jdbc.driver}") String driver;
+    @Value("${jdbc.url}") String url;
+    @Value("${jdbc.username}") String username;
+    @Value("${jdbc.password}") String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+
+        config.setDriverClassName(driver);
+        config.setJdbcUrl(url);
+        config.setUsername(username);
+        config.setPassword(password);
+
+        HikariDataSource ds = new HikariDataSource(config);
+        return ds;
+    }
 }
