1a1924ba6523700808f7d9b0d673272c627269b4
Jeong Jihee <102722507+dev-9hee@users.noreply.github.com>
2024-09-24 23:53:39 +0900

    20240924 전화번호목록 문제

---
+++ 04_java/algorithmstudy/src/study/전화번호목록/Solution.java
@@ -0,0 +1,23 @@
+package study.전화번호목록;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public boolean solution(String[] phone_book) {
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < phone_book.length; i++) {
+            set.add(phone_book[i]);
+        }
+
+        for (int i = 0; i < phone_book.length; i++) {
+            for (int j = 0; j < phone_book[i].length(); j++) {
+                if (set.contains(phone_book[i].substring(0, j))) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
